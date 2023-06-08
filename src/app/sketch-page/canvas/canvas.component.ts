0034ebf5505e8dbccd7a5df43a4853036b6c3b08
Samito19 <samiamsaf1902@gmail.com>
2023-06-08 09:55:19 -0400

    Removed unecessary code

--- src/app/sketch-page/canvas/canvas.component.ts
+++ src/app/sketch-page/canvas/canvas.component.ts
@@ -1,21 +1,18 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Store } from '@ngrx/store';
 import p5 from 'p5';
 import { connectCanvasName } from './canvas-component.actions';
 import { CanvasComponentService } from './canvas.component.service';
-import { Subscription, of } from 'rxjs';
 
 @Component({
   selector: 'app-canvas',
   templateUrl: './canvas.component.html',
   styleUrls: ['./canvas.component.scss'],
 })
-export class CanvasComponent implements OnInit, OnDestroy {
+export class CanvasComponent implements OnInit {
   canvas: p5;
-  stringCanvas: string = '';
-  sub$ = of('something we want');
-  subscription?: Subscription;
+
   constructor(
     private store: Store,
     private canvasComponentService: CanvasComponentService,
@@ -30,13 +27,8 @@ export class CanvasComponent implements OnInit, OnDestroy {
       })
     );
     this.canvasComponentService.init(param);
-    this.subscription = this.sub$.subscribe((r) => console.log(r));
   }
-  ngOnDestroy() {
-    if (this.subscription) {
-      this.subscription.unsubscribe();
-    }
-  }
+
   private _drawingUpdateHandler = (drawings: any): void =>
     drawings.forEach((drawing: number[]) =>
       this.canvas.line(drawing[0], drawing[1], drawing[2], drawing[3])
